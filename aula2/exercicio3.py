d24b4bf70d98613fb744d2ad1a9f9dbafab0fa26
claraAna309 <anaclarapereira5052@gmail.com>
2026-02-24 21:52:23 -0300

    Commit do exercicio3

---
+++ aula2/exercicio3.py
@@ -0,0 +1,11 @@
+"""
+Implemente o programa que calcule a media aritmetrica
+de duas notas bimestrais fornecidas pelo usuario.
+"""
+
+nota1= float(input("Digite a primeira nota: "))
+nota2= float(input("Digite a segunda nota: "))
+
+media= (nota1+nota2)/2
+
+print(f"A media das notas {nota1} e {nota2} é \n = {media}")
